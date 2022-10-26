8e0d2a3c7adddc878266cd7c63dafab92af46297
erirfanhamid <erirfanhamid@gmail.com>
2022-10-26 10:32:23 +0530

    my first commity

---
+++ test/hack1.py
@@ -0,0 +1,8 @@
+a=3
+b=5
+sum=a+b
+print("sum of two numbers is:",sum)
+difference=a-b
+print("\ndifference of two numbers is:",difference)
+product=a*b
+print("\n product of two numbers is:", product)
